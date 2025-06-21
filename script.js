ee0ee2903a3a25f20ad5a4b03c26eac425c84f67
odunsi <opemipoodunsi428@gmail.com>
2025-06-21 02:39:24 +0100

    Write the logic to play a game round
    i wrote the logic to play a round of the game and
    then output wether or no the user has won, lost or
    it is a draw

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Return either one of "rock" "paper" or "scissors" 
+// Return either one of "rock" "paper" or "scissors" from the computer
 function getComputerChoice () {
     // Use Math.random() to get either one of 0, 1 or 2
     let computerChoice;
@@ -15,14 +15,51 @@ function getComputerChoice () {
     }
     else {
         computerChoice = "scissors";
-        return computerChoice
+        return computerChoice;
     }
 }
 
+// Return either one of "rock" "paper" or "scissors" from the user
 function getHumanChoice() {
     // Prompt user for an input of either "rock" "paper" or "scissors"
     let humanChoice = prompt("rock, paper or scissors?");
     return humanChoice;
 }
 
-console.log(getHumanChoice());
+// Declare the players score variables
+let humanScore = 0;
+let computerScore = 0;
+
+// Play a single round
+function playRound(humanChoice, computerChoice) {
+    // Make humanChoice case insensitive
+    humanChoice = humanChoice.toLowerCase()
+
+    // Check for a winner
+    if (humanChoice === computerChoice) {
+        humanScore;
+        computerScore;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock") {
+        humanScore += 1;
+    }
+    else {
+        computerScore += 1;
+    }
+
+    //  Print out round result
+    if (humanScore > computerScore) {
+        console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+    }
+    else if (computerScore > humanScore) {
+        console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+    }
+    else {
+        console.log("Draw!")
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
